54cc86ddbb93a8408ed275982867b29f0a7c5bcf
Faisal <faisalanderson1@gmail.com>
2016-02-23 20:11:37 +0000

    Add all files

---
+++ js/shuffleScript.js
@@ -0,0 +1,14 @@
+$(function(){
+	
+	// container is the DOM element;
+	// userText is the textbox
+	
+	var container = $('*[data-shuffle="1"]');
+		
+	
+	// Shuffle the contents of container
+	container.shuffleLetters();
+
+	
+});
+
