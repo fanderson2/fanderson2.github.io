ec68c6b0cc2ea628e31e114e83a97cf7a95008b5
Faisal <faisalanderson1@gmail.com>
2016-03-18 15:40:53 +0000

    Update 3

---
+++ lib/sonnyt-Tweetie-d2be9a3/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'yMnUmMOah0FBZzSemK6ZaEz9W');
+    define('CONSUMER_SECRET', '2XSUbQaw81AhgUivGBsUfCkrh7PxYoXwZTJeAGCFtm4Itimd9k');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '710851456765698048-LAi7GEZVgxxtWPsY0Mdg7IaVlupS1nZ');
+    define('ACCESS_SECRET', 'i5W2vnGx48sW11Bws5sxU4bNIVt2HFvqZoWa3Q8rrlUzB');
